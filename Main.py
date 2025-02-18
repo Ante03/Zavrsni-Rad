7f94af07589869ab8125cfc15c1695cad62b8aae
Ante03 <spajica02@gmail.com>
2025-02-18 12:25:48 +0100

    first commit, add UI

---
+++ Main.py
@@ -0,0 +1,52 @@
+
+import pygame as p
+import ChessEngine
+
+WIDTH = HEIGHT = 400
+DIMENSION = 8
+SQ_SIZE = HEIGHT // DIMENSION
+IMAGES = {}
+MAX_FPS = 15
+
+def main():
+    p.init()
+    screen = p.display.set_mode((WIDTH, HEIGHT))
+    p.display.set_caption("Ante Chess")
+    gs = ChessEngine.GameState()
+    loadImages()
+    running = True
+    while running:
+        for event in p.event.get():
+            if event.type == p.QUIT:
+                running = False
+        drawGameState(screen, gs)
+        p.display.flip()
+
+
+def loadImages():
+    pieces = ['wP', 'wR', 'wN', 'wB', 'wK', 'wQ', 'bP', 'bR', 'bN', 'bB', 'bK', 'bQ']
+    for piece in pieces:
+        IMAGES[piece] = p.image.load("Pieces/" + piece + ".svg")
+
+def drawGameState(screen, gs):
+    drawBoard(screen)
+    drawPieces(screen, gs.board)
+
+def drawBoard(screen):
+    colors = [p.Color("white"), p.Color("dark green")]
+    for r in range(DIMENSION):
+        for c in range(DIMENSION):
+            color = colors[((r + c) % 2)]
+            p.draw.rect(screen, color, p.Rect(c*SQ_SIZE, r*SQ_SIZE, SQ_SIZE, SQ_SIZE))
+
+def drawPieces(screen, board):
+    for r in range(DIMENSION):
+        for c in range(DIMENSION):
+            piece = board[r][c]
+            if piece != "--":
+                screen.blit(IMAGES[piece], p.Rect(c*SQ_SIZE, r*SQ_SIZE, SQ_SIZE, SQ_SIZE))
+
+
+
+if __name__ == "__main__":
+    main()
